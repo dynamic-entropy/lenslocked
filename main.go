1cda68611602fb87ca2e1b81a79816b20f4fa989
Rahul Chauhan <omrahulchauhan@gmail.com>
2021-04-15 19:39:09 +0530

    Changed server to use gorrial/mux router

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
-
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Hello World!</h1>")
 }
 
-func main(){
-	http.HandleFunc("/", handlerFunc);
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello World! Contact me at <me@domain.com></h1>")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
 }
