81a98b61bdeeb1fd6cec2236550b3d98aa6de765
Rahul Chauhan <omrahulchauhan@gmail.com>
2021-04-16 11:55:09 +0530

    Added views and updated main to use them

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Data struct {
+	Name    string
+	Surname string
+	Domain  string
+}
+
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello World!</h1>")
+	data := Data{Name: "Rahul"}
+	if err := homeTemplate.Execute(w, data); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World! Contact me at <me@domain.com></h1>")
+	w.Header().Set("Content-Type", "text/html")
+	data := Data{"Rahul", "Chauhan", "Golang"}
+	if err := contactTemplate.Execute(w, data); err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.html")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.html")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
